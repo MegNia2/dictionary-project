4d5fc4a23bc1302942c6fa0df3efed2b26427f7e
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-10-07 19:30:31 +0100

    Installed axios and fetched api for dictionary to return definition

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+
+  function showDefinition(response) {
+    console.log(response.data);
+  }
+
   function handleResponse(event) {
     event.preventDefault();
     alert(`You are searching for ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(showDefinition);
   }
 
   function handleChange(event) {
