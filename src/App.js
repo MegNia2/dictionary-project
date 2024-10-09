cf6006d2c1e811101a20f88206cef87d20ca998e
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-10-09 20:33:50 +0100

    Added icon in header and edited margin in App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+import { BsBook } from "react-icons/bs";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+
+      <h1>
+        <BsBook />
+      </h1>
+
       <Dictionary />
+      <footer>
+        <small>
+          {" "}
+          Coded by{" "}
+          <a
+            href="https://github.com/MegNia2/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Megan Gee
+          </a>
+          , hosted on{" "}
+          <a
+            href="https://megnia-dictionary.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
