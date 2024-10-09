515a2b36735168c1dc23c8050941072ecc824efb
MegNia2 <165702683+MegNia2@users.noreply.github.com>
2024-10-09 20:34:24 +0100

    Created css for Meanings

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,16 +1,18 @@
 import React from "react";
+import "./Meanings.css";
 
 export default function Meanings(props) {
   console.log(props.meanings);
   return (
     <div className="Meanings">
-      <h2>{props.meanings.partOfSpeech}</h2>
+      <h4>{props.meanings.partOfSpeech}</h4>
       <div>
         {props.meanings.definitions.map(function (definitions, index) {
           return (
             <div>
               <div key={index}>
-                <p>{definitions.definition}</p>
+                <strong>{definitions.definition}</strong>
+                <br />
                 <em>{definitions.example}</em>
               </div>
             </div>
